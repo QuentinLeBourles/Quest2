36ec9cbda51fda10ec27ceafe211fbda1ba56bd3
QuentinLeBourles <kahi.qlb@gmail.com>
2018-10-08 17:30:53 +0200

    LOLOLOL

---
+++ src/History.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class History extends Component {
+    render() {
+        return (
+            <div>
+                <p>Notre histoire? Bon assis toi copain, il s'en ai passer des choses cet nuit là ...</p>
+            </div>
+
+        );
+    }
+}
+export default History;
---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <p>Bienvenue les copains, vous voulez pas un ptit sucre pour checké le travail des autres?</p>
+            </div>
+
+        );
+    }
+}
+export default Home;
