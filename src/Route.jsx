2a1ac6b3a5316bdd11061a2679482c8a44bf24d2
QuentinLeBourles <kahi.qlb@gmail.com>
2018-10-08 17:48:07 +0200

    2

--- src/Route.jsx
+++ src/Route.jsx
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
 import Home from './Home';
 import History from './History';
+import './App.css'
 
 class Routing extends Component {
     render() {
         return (
             <BrowserRouter>
               <div>
-                <NavLink exact to="/"> Home </NavLink>
-                <NavLink to="/page-de-contact"> History </NavLink>
+                <NavLink exact to="/" activeClassName="selected"> Home </NavLink>
+                <NavLink to="/page-de-contact" activeClassName="selected"> History </NavLink>
 
                 <Switch>
                   <Route exact path="/" component={Home} />
